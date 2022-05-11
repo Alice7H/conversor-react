5ab886c21504c594c8e2f85e657b4ec1f307fdf7
Alice Hata <alicehata01@hotmail.com>
2022-05-10 23:33:37 -0300

    Update README.md
    Updated readme.md
    Added banner image
    Removed logo, documents about tests and service worker.

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ function App() {
   return (
     <div className="App">
       <h1>Conversor de moedas</h1>
+      <p>BRL = Real</p>
+      <p>USD = Dólar Americano</p>
+      <p>CAD = Dólar Canadense</p>
+      <p>EUR = Euro</p>
+
+
       <div className="linha">
-        <Conversor  moedaA="USD" moedaB="BRL"/>
-        <Conversor  moedaA="BRL" moedaB="USD"/>
+        <Conversor moedaA="USD" moedaB="BRL"/>
+        <Conversor moedaA="BRL" moedaB="USD"/>
       </div>
 
       <div className="linha">
-        <Conversor  moedaA="CAD" moedaB="BRL"/>
-        <Conversor  moedaA="BRL" moedaB="CAD"/>
+        <Conversor moedaA="CAD" moedaB="BRL"/>
+        <Conversor moedaA="BRL" moedaB="CAD"/>
       </div>
 
       <div className="linha">
-        <Conversor  moedaA="EUR" moedaB="BRL"/>
-        <Conversor  moedaA="BRL" moedaB="EUR"/>
+        <Conversor moedaA="EUR" moedaB="BRL"/>
+        <Conversor moedaA="BRL" moedaB="EUR"/>
       </div>
     </div>
   );
